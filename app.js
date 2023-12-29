24cc1e49a81a72b03ee16b0d2869ab324ea8656f
dggau <dggaudet11@gmail.com>
2023-12-28 20:36:31 -0500

    Field Studies added

--- app.js
+++ app.js
@@ -8,3 +8,12 @@ function showContent3(){
     //window.location.href = ("https://i.ibb.co/ZX0Xt4n/IMG-0968.jpg");
     window.open('https://i.ibb.co/ZX0Xt4n/IMG-0968.jpg', '_blank');
 }
+function showContent4(){
+    window.open('https://miro.medium.com/v2/resize:fit:1200/1*d8DyNLUIa8xo5rGrO-2FSg.jpeg', '_blank');
+}
+function showContent5(){
+    window.open('https://www.water-pollutionsolutions.com/image-files/trash-boom-400x400.webp', '_blank');
+}
+function showContent6(){
+    window.open('https://apilgriminnarnia.files.wordpress.com/2015/12/griswold-christmas-tree-car.jpg', '_blank');
+}
