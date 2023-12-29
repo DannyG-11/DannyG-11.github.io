1f3670ab50dc6abc908d200d3752c847c5a0fba0
dggau <dggaudet11@gmail.com>
2023-12-29 15:51:48 -0500

    Added tabs

--- app.js
+++ app.js
@@ -17,3 +17,25 @@ function showContent5(){
 function showContent6(){
     window.open('https://apilgriminnarnia.files.wordpress.com/2015/12/griswold-christmas-tree-car.jpg', '_blank');
 }
+
+// Tabs start
+
+const tabs = document.querySelectorAll('[data-tab-target')
+const tabContents = document.querySelectorAll('[data-tab-content')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {
+            tabContent.classList.remove('active')
+        })
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        })
+        tab.classList.add('active')
+        target.classList.add('active')
+    })
+
+})
+
+// Tabs end
